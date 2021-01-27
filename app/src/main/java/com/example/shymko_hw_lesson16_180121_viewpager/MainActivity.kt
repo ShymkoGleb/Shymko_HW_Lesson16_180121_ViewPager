6975ef4c10adaea78d0abdf3a3f54078791a1abc
ShymkoGleb <71500261+ShymkoGleb@users.noreply.github.com>
2021-01-28 01:03:37 +0200

    delete bittons

--- app/src/main/java/com/example/shymko_hw_lesson16_180121_viewpager/MainActivity.kt
+++ app/src/main/java/com/example/shymko_hw_lesson16_180121_viewpager/MainActivity.kt
@@ -5,27 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
+/*
+
+Створити додаток з використанням ViewPager,
+як контент використати список країн,
+у заголовку повинна бути назва країни,
+у fragment детальна інформація.
+Обов’язкові поля в детальній інформації:
+назва, населення, площа, густота населення (рахується як площа поділена на населення).
+
+*/
 class MainActivity : AppCompatActivity() {
+    companion object {
+        var isCountryAdded: Boolean = false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Country.listOfCountries.add(Country("Ukraine", 40000000, 55555, 555, "adasdds"))
-        onStart()
-        Country.listOfCountries.add(Country("Egypt", 80000000, 22222, 3333, "sadadsad"))
-        onStart()
-        Country.listOfCountries.add(Country("Kair", 30000000, 22222, 3333, "sadadsad"))
-        onStart()
-        Country.listOfCountries.add(Country("Laos", 40000000, 22222, 3333, "sadadsad"))
-        onStart()
-
+        addCountries(isCountryAdded)
 
         val adapter = ViewPagerAdapter(Country.listOfCountries)
         vpViewPager.adapter = adapter
-        //  adapter.notifyItemInserted(Country.listOfCountries.size - 1)
-
         TabLayoutMediator(tabTabLayout, vpViewPager) { tab, position ->
-            tab.text = " Tab Pos = ${position + 1}"
+            tab.text = "Country ${position + 1}"
         }.attach()
+    }
+
+    private fun addCountries(flag: Boolean) {
+        if (!flag) {
+            Country.listOfCountries.add(Country("Ukraine", 40000000, 55555, 555, "adasdds"))
+            Country.listOfCountries.add(Country("Egypt", 80000000, 22222, 3333, "sadadsad"))
+            Country.listOfCountries.add(Country("Kair", 30000000, 22222, 3333, "sadadsad"))
+            Country.listOfCountries.add(Country("Laos", 40000000, 22222, 3333, "sadadsad"))
+            isCountryAdded = true
+        }
     }
 }
